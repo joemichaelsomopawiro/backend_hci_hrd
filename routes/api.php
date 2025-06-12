ad7f0b2f0a89eae5b6822a9f368fb040d077b86d
joemichaelsomopawiro <2281013@unai.edu>
2025-06-12 14:54:19 +0700

    feat(api): add leave and attendance management routes
    Add new API routes for leave quotas, leave requests, and attendance management including CRUD operations and specific actions like approve/reject for leave requests and check-in/check-out for attendance

--- routes/api.php
+++ routes/api.php
@@ -14,3 +14,30 @@ Route::delete('/employees/{employeeId}/employment-histories/{historyId}', [Emplo
 Route::delete('/employees/{employeeId}/promotion-histories/{promotionId}', [EmployeeController::class, 'deletePromotionHistory']);
 Route::delete('/employees/{employeeId}/trainings/{trainingId}', [EmployeeController::class, 'deleteTraining']);
 Route::delete('/employees/{employeeId}/benefits/{benefitId}', [EmployeeController::class, 'deleteBenefit']);
+
+// Leave Quota Routes
+Route::prefix('leave-quotas')->group(function () {
+    Route::get('/', [App\Http\Controllers\LeaveQuotaController::class, 'index']);
+    Route::post('/', [App\Http\Controllers\LeaveQuotaController::class, 'store']);
+    Route::get('/{id}', [App\Http\Controllers\LeaveQuotaController::class, 'show']);
+    Route::put('/{id}', [App\Http\Controllers\LeaveQuotaController::class, 'update']);
+    Route::delete('/{id}', [App\Http\Controllers\LeaveQuotaController::class, 'destroy']);
+});
+
+// Leave Request Routes
+Route::prefix('leave-requests')->group(function () {
+    Route::get('/', [App\Http\Controllers\LeaveRequestController::class, 'index']);
+    Route::post('/', [App\Http\Controllers\LeaveRequestController::class, 'store']);
+    Route::put('/{id}/approve', [App\Http\Controllers\LeaveRequestController::class, 'approve']);
+    Route::put('/{id}/reject', [App\Http\Controllers\LeaveRequestController::class, 'reject']);
+});
+
+// Attendance Routes
+Route::prefix('attendances')->group(function () {
+    Route::get('/', [App\Http\Controllers\AttendanceController::class, 'index']);
+    Route::post('/', [App\Http\Controllers\AttendanceController::class, 'store']);
+    Route::post('/check-in', [App\Http\Controllers\AttendanceController::class, 'checkIn']);
+    Route::post('/check-out', [App\Http\Controllers\AttendanceController::class, 'checkOut']);
+    Route::get('/summary', [App\Http\Controllers\AttendanceController::class, 'workHoursSummary']);
+    Route::get('/dashboard', [App\Http\Controllers\AttendanceController::class, 'dashboard']);
+});
